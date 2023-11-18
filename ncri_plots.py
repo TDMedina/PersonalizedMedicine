55d4c998e954ea2bfe79538f0259d23cfe582f74
TDMedina <tylerdanmedina@gmail.com>
2023-11-18 21:39:32 +0000

    Updated to use pandas extensions.

--- ncri_plots.py
+++ ncri_plots.py
@@ -8,8 +8,17 @@ import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
 
+BAND_DICT = {"0-49": ["Under 1 year", "1 - 4 years", "5 - 9 years", "10 - 14 years",
+                      "15 - 19 years", "20 - 24 years", "25 - 29 years",
+                      "30 - 34 years", "35 - 39 years", "40 - 44 years",
+                      "45 - 49 years"],
+             "50-64": ["50 - 54 years", "55 - 59 years", "60 - 64 years"],
+             "65-74": ["65 - 69 years", "70 - 74 years"],
+             "75+": ["75 - 79 years", "80 - 84 years", "85 years and over"]}
+AGE_BANDS = [band for bands in BAND_DICT.values() for band in bands]
 
-@register_dataframe_accessor("survival")
+
+@register_dataframe_accessor("survival_tools")
 class SurvivalData:
     def __init__(self, pandas_obj):
         self._obj = pandas_obj
@@ -31,34 +40,82 @@ class SurvivalData:
                                        "(except NMSC) in Ireland"),
                                 width=750, height=500,
                                 template="plotly_white")
-        survival_plot.update_layout(legend_title="Date range")
+        survival_plot.update_layout(legend_title="Date range", height=450, width=600)
         survival_plot.update_yaxes(tickformat=".0%", range=[0, 1])
         return survival_plot
 
 
-# def read_survival(survival_data):
-#     survival_data = pd.read_csv(survival_data, sep=",")
-#     survival_data = survival_data.loc[survival_data.Dates != "2009-2013"]
-#     survival_data = (survival_data
-#                      .sort_values(by="Time (years)")
-#                      .sort_values(by="Dates", ascending=False, kind="stable"))
-#     survival_data["Net survival"] = survival_data["Net survival"] / 100
-#     return survival_data
-#
-#
-# def plot_survival(survival_data):
-#     survival_plot = px.line(survival_data, x="Time (years)", y="Net survival",
-#                             color="Dates", markers=True,
-#                             title=("Survival: All invasive cancers "
-#                                    "(except NMSC) in Ireland"),
-#                             width=750, height=500,
-#                             template="plotly_white")
-#     survival_plot.update_layout(legend_title="Date range")
-#     survival_plot.update_yaxes(tickformat=".0%", range=[0, 1])
-#     return survival_plot
+@register_dataframe_accessor("population_tools")
+class PopulationData:
+    def __init__(self, pandas_obj):
+        self._obj = pandas_obj
+
+    @staticmethod
+    def read_csv(population_csv, correct_errant_years=True):
+        pop_data = pd.read_csv(population_csv, sep=",", usecols=[1, 2, 3, 5])
+
+        cols = list(pop_data.columns)
+        cols[-1] = "Population"
+        pop_data.columns = cols
+
+        pop_data["Year"] = [datetime(x, 1, 1) for x in pop_data.Year]
+        pop_data = pop_data.loc[(datetime(1994, 1, 1) <= pop_data.Year)
+                                & (pop_data.Year < datetime(2020, 1, 1))]
+        pop_data["Population"] = pop_data["Population"] * 1000
+
+        pop_data = (pop_data
+                    .sort_values("Age Group",
+                                 key=lambda x: [y.replace("Under", "0") for y in x])
+                    .sort_values("Sex", kind="stable")
+                    .sort_values("Year", kind="stable"))
+        pop_data.set_index(["Year", "Sex", "Age Group"], inplace=True)
+        pop_data = pop_data.loc[idx[datetime(1994, 1, 1):datetime(2019, 1, 1), :, AGE_BANDS]]
+        pop_data.rename(index={"Both sexes": "Both"}, level="Sex", inplace=True)
+
+        if correct_errant_years:
+            for i in range(2012, 2017):
+                subset = pop_data.loc[idx[datetime(i, 1, 1), :, "1 - 4 years"]]
+                correction = (subset.loc["Both"] - subset.loc["Male"]).Population
+                pop_data.loc[idx[datetime(i, 1, 1), "Female", "1 - 4 years"]] = correction
+
+        return pop_data
+
+    def aggregate_cancer_age_groups(self):
+        # population_data = self._obj.loc[idx[:, "Both", :]]
+        aggregate = pd.DataFrame(columns=["Year", "Sex", "Population", "Age Group"])
+        for i, (agg_band, bands) in enumerate(BAND_DICT.items()):
+            sub_agg = self._obj.loc[idx[:, :, bands]].groupby(["Year", "Sex"]).agg(sum)
+            sub_agg["Age Group"] = agg_band
+            sub_agg.reset_index(inplace=True)
+            aggregate = pd.concat([aggregate, sub_agg])
+        aggregate.set_index(["Year", "Sex", "Age Group"], inplace=True)
+        aggregate.sort_index(inplace=True)
+        return aggregate
+
+    def add_proportions(self):
+        data = self._obj
+        year_aggregated = data.groupby(["Year", "Sex"]).agg(sum)
+        data["Proportion"] = [pop / year_aggregated.loc[idx[year, sex]].Population
+                              for (year, sex, age), pop in data.itertuples()]
+        return data
+
+    def plot(self, normalized=False, aggregate_cancer_groups=False):
+        data = self._obj
+        if aggregate_cancer_groups:
+            data = data.population_tools.aggregate_cancer_age_groups()
+        if normalized:
+            data = data.population_tools.add_proportions()
+            y = "Proportion"
+        else:
+            y = "Population"
+        data = data.loc[idx[:, "Both", :]]
+        data = data.reset_index()
+        fig = px.bar(data, x="Year", y=y, color="Age Group")
+        fig.update_layout(bargap=0)
+        return fig
 
 
-@register_dataframe_accessor("age_incidence")
+@register_dataframe_accessor("age_incidence_tools")
 class AgeIncidenceData:
     def __init__(self, pandas_obj):
         self._obj = pandas_obj
@@ -66,11 +123,14 @@ class AgeIncidenceData:
     @staticmethod
     def read_csv(incidence_data):
         data = pd.read_csv(incidence_data, sep=",")
-        data["Year"] =
+        cols = list(data.columns)
+        cols[0] = "Age Group"
+        data.columns = cols
+        data["Year"] = [datetime(x, 1, 1) for x in data.Year]
         data = (data
                 .sort_values(by="Year")
-                .sort_values(by="Age band", ascending=False, kind="stable"))
-        data.set_index(["Age band"], inplace=True)
+                .sort_values(by="Age Group", ascending=False, kind="stable"))
+        data.set_index(["Year", "Age Group"], inplace=True)
         return data
 
     def plot(self, raw_count=False, combined=False):
@@ -82,15 +142,17 @@ class AgeIncidenceData:
             y_title = "Cases per year"
         else:
             y = "Crude rate"
-            y_title = "Cases per 100,000 per year (crude rate)"
-        incidence_plot = px.line(self._obj, x="Year", y=y,
-                                 color=self._obj.index.values,
+            y_title = "Cases per 100,000 per year"
+        data = self._obj.reset_index()
+        incidence_plot = px.line(data, x="Year", y=y,
+                                 color="Age Group",
                                  markers=True, width=750, height=500,
                                  title=("Incidence by age: All invasive cancers "
                                         "(except NMSC) in Ireland"),
                                  template="plotly_white")
         incidence_plot = incidence_plot.update_layout(yaxis_title=y_title,
-                                                      legend_title="Age band")
+                                                      legend_title="Age Group")
+        incidence_plot.update_xaxes(dtick="M60")
         return incidence_plot
 
     def _plot_combined(self):
@@ -108,119 +170,45 @@ class AgeIncidenceData:
 
         age_plot.update_traces(showlegend=False, col=2)
         age_plot.update_layout(title=raw.layout.title.text,
-                               legend=dict(
-                                   title="Age band",
-                                   # xanchor="center",
-                                   # orientation="h",
-                                   # x=0.5
-                                   ),
+                               legend_title="Age Group",
                                template="plotly_white")
+        age_plot.update_xaxes(dtick="M60")
+
         age_plot.layout.annotations[0].update(xanchor="left", xshift=-190)
         age_plot.layout.annotations[1].update(xanchor="left", xshift=-190)
-        # age_plot.update_annotations(xanchor="left", xshift=-190)
+
+        age_plot.update_layout(height=450, width=900)
 
         return age_plot
 
-# def read_age_incidence(incidence_data):
-#     incidence_data = pd.read_csv(incidence_data, sep=",")
-#     incidence_data = (incidence_data
-#                       .sort_values(by="Year")
-#                       .sort_values(by="Age band", ascending=False, kind="stable"))
-#     incidence_data.set_index(["Age band"], inplace=True)
-#     return incidence_data
-#
-#
-# def plot_age_incidence(age_incidence_data, raw_count=False, combined=False):
-#     if combined:
-#         incidence_plot = _plot_combined_age_incidence(age_incidence_data)
-#         return incidence_plot
-#     if raw_count:
-#         y = "Case numbers"
-#         y_title = "Cases per year"
-#     else:
-#         y = "Crude rate"
-#         y_title = "Cases per 100,000 per year (crude rate)"
-#     incidence_plot = px.line(age_incidence_data, x="Year", y=y,
-#                              color=age_incidence_data.index.values,
-#                              markers=True, width=750, height=500,
-#                              title=("Incidence by age: All invasive cancers "
-#                                     "(except NMSC) in Ireland"),
-#                              template="plotly_white")
-#     incidence_plot = incidence_plot.update_layout(yaxis_title=y_title,
-#                                                   legend_title="Age band")
-#     return incidence_plot
-#
-#
-# def _plot_combined_age_incidence(age_incidence_data):
-#     raw = plot_age_incidence(age_incidence_data, raw_count=True)
-#     rel = plot_age_incidence(age_incidence_data, raw_count=False)
-#
-#     age_plot = make_subplots(rows=1, cols=2,
-#                              subplot_titles=["a) Cases per age band",
-#                                              "b) Cases per 100K in age band"],
-#                              x_title="Year",
-#                              y_title="Cases")
-#     for i, plot in enumerate([raw, rel], start=1):
-#         for trace in plot.data:
-#             age_plot.add_trace(trace, row=1, col=i)
-#
-#     age_plot.update_traces(showlegend=False, col=2)
-#     age_plot.update_layout(title=raw.layout.title.text,
-#                            legend=dict(
-#                                title="Age band",
-#                                # xanchor="center",
-#                                # orientation="h",
-#                                # x=0.5
-#                                ),
-#                            template="plotly_white")
-#     age_plot.layout.annotations[0].update(x=0.07)
-#     age_plot.layout.annotations[1].update(x=0.68)
-#
-#     # age_plot.update_xaxes(title_text="xaxis 1 title", row=1, col=1)
-#
-#     return age_plot
+    def merge_with_population_table(self, population_df):
+        pop_data = population_df.population_tools.aggregate_cancer_age_groups()
+        pop_data = pop_data.loc[idx[:, "Both", :]].groupby(["Year", "Age Group"]).agg(sum)
+        data = pd.merge(self._obj, pop_data, left_index=True, right_index=True)
+        data["Relative numbers"] = data["Case numbers"] / data["Population"] * 100000
+        return data
 
 
-@register_dataframe_accessor("sex_incidence")
+@register_dataframe_accessor("sex_incidence_tools")
 class SexIncidenceData:
     def __init__(self, pandas_obj):
         self._obj = pandas_obj
 
-    @classmethod
-    def read_data(cls, sex_incidence_csv, population_csv):
-        incidence = cls._read_sex_incidence_csv(sex_incidence_csv)
-        population = cls._read_population_csv(population_csv)
-        data = cls._merge_incidence_and_population_tables(incidence, population)
-        return data
-
     @staticmethod
-    def _read_sex_incidence_csv(sex_incidence_csv):
+    def read_csv(sex_incidence_csv):
         incidence = pd.read_csv(sex_incidence_csv)
+        incidence["Year"] = [datetime(x, 1, 1) for x in incidence.Year]
         incidence.set_index(["Year", "Sex"], inplace=True)
         incidence.rename(index={"Males": "Male", "Females": "Female"},
                          level="Sex", inplace=True)
         return incidence
 
-    @staticmethod
-    def _read_population_csv(population_csv, drop_subgroups=True):
-        pop_data = pd.read_csv(population_csv, sep=",", usecols=[1, 2, 3, 5])
-        cols = list(pop_data.columns)
-        cols[-1] = "Population"
-        pop_data.columns = cols
-        pop_data["Population"] = pop_data["Population"] * 1000
-        if drop_subgroups:
-            pop_data = pop_data.loc[pop_data["Age Group"] == "All ages"]
-            pop_data.drop("Age Group", axis=1, inplace=True)
-            pop_data.set_index(["Year", "Sex"], inplace=True)
-        else:
-            pop_data.set_index(["Year", "Sex", "Age Group"], inplace=True)
-        pop_data.sort_index(inplace=True)
-        pop_data.rename(index={"Both sexes": "Both"}, level="Sex", inplace=True)
-        return pop_data
-
-    @staticmethod
-    def _merge_incidence_and_population_tables(incidence, population):
-        data = pd.merge(incidence, population, left_index=True, right_index=True)
+    def merge_with_population_table(self, population_df):
+        # pop_data = population_df.loc[population_df["Age Group"] == "All ages"]
+        pop_data = population_df.groupby(["Year", "Sex"]).agg(sum)
+        # pop_data.drop("Age Group", axis=1, inplace=True)
+        # pop_data.set_index(["Year", "Sex"], inplace=True)
+        data = pd.merge(self._obj, pop_data, left_index=True, right_index=True)
         data["Relative numbers"] = data["Case numbers"] / data["Population"] * 100000
         return data
 
@@ -241,6 +229,7 @@ class SexIncidenceData:
                                         "(except NMSC) in Ireland"),
                                  template="plotly_white")
         incidence_plot.update_layout(yaxis_title=y_title)
+        incidence_plot.update_xaxes(dtick="M60")
         return incidence_plot
 
     def _plot_combined(self):
@@ -258,100 +247,27 @@ class SexIncidenceData:
 
         sex_plot.update_traces(showlegend=False, col=2)
         sex_plot.update_layout(title=raw.layout.title.text,
-                               legend=dict(
-                                   title="Sex",
-                                   # xanchor="center",
-                                   # orientation="h",
-                                   # x=0.5
-                                   ),
+                               legend_title="Sex",
                                template="plotly_white")
+        sex_plot.update_xaxes(dtick="M60")
 
         sex_plot.layout.annotations[0].update(xanchor="left", xshift=-190)
         sex_plot.layout.annotations[1].update(xanchor="left", xshift=-190)
-        # sex_plot.update_annotations(xanchor="left", xshift=-190)
-        return sex_plot
 
-# def read_total_incidence(total_incidence_data):
-#     incidence = pd.read_csv(total_incidence_data)
-#     incidence.set_index(["Year", "Sex"], inplace=True)
-#     incidence.rename(index={"Males": "Male", "Females": "Female"},
-#                      level="Sex", inplace=True)
-#     return incidence
-#
-#
-# def read_population_data(population_data, drop_subgroups=True):
-#     pop_data = pd.read_csv(population_data, sep=",", usecols=[1, 2, 3, 5])
-#     cols = list(pop_data.columns)
-#     cols[-1] = "Population"
-#     pop_data.columns = cols
-#     pop_data["Population"] = pop_data["Population"]*1000
-#     if drop_subgroups:
-#         pop_data = pop_data.loc[pop_data["Age Group"] == "All ages"]
-#         pop_data.drop("Age Group", axis=1, inplace=True)
-#         pop_data.set_index(["Year", "Sex"], inplace=True)
-#     else:
-#         pop_data.set_index(["Year", "Sex", "Age Group"], inplace=True)
-#     pop_data.sort_index(inplace=True)
-#     pop_data.rename(index={"Both sexes": "Both"}, level="Sex", inplace=True)
-#     return pop_data
-#
-#
-# # def aggregate_cancer_age_band(population_data):
-#
-#
-#
-# def merge_incidence_and_population_tables(incidence, population):
-#     data = pd.merge(incidence, population, left_index=True, right_index=True)
-#     data["Relative numbers"] = data["Case numbers"] / data["Population"] * 100000
-#     return data
-#
-#
-# def plot_total_incidence(total_incidence_data, raw_count=True):
-#     if raw_count:
-#         y = "Case numbers"
-#         y_title = "Cases per year"
-#     else:
-#         y = "Relative numbers"
-#         y_title = "Cases per 100,000 total population per year"
-#     data = total_incidence_data.reset_index()
-#     incidence_plot = px.line(data, x="Year", y=y, color="Sex",
-#                              markers=True, width=750, height=500,
-#                              title=("Total incidence: All invasive cancers "
-#                                     "(except NMSC) in Ireland"),
-#                              template="plotly_white")
-#     incidence_plot.update_layout(yaxis_title=y_title)
-#     return incidence_plot
+        sex_plot.update_layout(height=450, width=900)
+
+        return sex_plot
 
 
 def main(age_incidence_csv, sex_incidence_csv, population_csv, survival_csv):
     age_incidence = AgeIncidenceData.read_csv(age_incidence_csv)
-    sex_incidence = SexIncidenceData.read_data(sex_incidence_csv, population_csv)
+    sex_incidence = SexIncidenceData.read_csv(sex_incidence_csv)
+    population = PopulationData.read_csv(population_csv)
+    age_incidence = age_incidence.age_incidence_tools.merge_with_population_table(population)
+    sex_incidence = sex_incidence.sex_incidence_tools.merge_with_population_table(population)
     survival = SurvivalData.read_csv(survival_csv)
 
-    age_plot = age_incidence.age_incidence.plot(combined=True)
-    sex_plot = sex_incidence.sex_incidence.plot(combined=True)
-    survival_plot = survival.survival.plot()
+    age_plot = age_incidence.age_incidence_tools.plot(combined=True)
+    sex_plot = sex_incidence.sex_incidence_tools.plot(combined=True)
+    survival_plot = survival.survival_tools.plot()
     return age_plot, sex_plot, survival_plot
-    #
-    #
-    #
-    #
-    #
-    # # age_incidence_data = read_age_incidence(age_incidence_data)
-    # age_incidence_data = AgeIncidence.read_age_incidence(age_incidence_csv)
-    # total_incidence_data = read_total_incidence(total_incidence_csv)
-    # # survival_data = read_survival(survival_data)
-    # survival_data = SurvivalData.read_survival(survival_csv)
-    # population_data = read_population_data(population_csv)
-    # total_incidence_data = merge_incidence_and_population_tables(total_incidence_data,
-    #                                                              population_data)
-    #
-    # age_incidence_plot = plot_age_incidence(age_incidence_data, raw_count=False)
-    # age_raw_incidence_plot = plot_age_incidence(age_incidence_data, raw_count=True)
-    # total_incidence_plot = plot_total_incidence(total_incidence_data, raw_count=False)
-    # total_raw_incidence_plot = plot_total_incidence(total_incidence_data, raw_count=True)
-    # survival_plot = plot_survival(survival_data)
-    # return ((age_incidence_data, total_incidence_data, survival_data),
-    #         (age_raw_incidence_plot, age_incidence_plot,
-    #          total_raw_incidence_plot, total_incidence_plot,
-    #          survival_plot))
